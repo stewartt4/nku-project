47777179a8b6e849b2ef3d8fb95b25103cd03e53
Tyler Stewart <stewartt4@nku.edu>
2014-04-01 00:23:23 +0000

    add to user controller skeleton

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,28 @@
 class UsersController < ApplicationController
   def index
-
+    @users = User.all
   end
 
   def new
-
+    @user = User.new
   end
 
   def edit
-
+    #@user = current_user
   end
 
   def update
-
+    #current_user.update_attributes!(user_params)
+    #redirect_to users_path, notice: "Successfully updated your profile!"
   end
 
   def destroy
+    
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit!
   end
 end
