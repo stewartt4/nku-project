9b0106942f803cf2c516aeb5e6d7dc2216664fe8
Tyler Stewart <stewartt4@nku.edu>
2014-04-05 15:42:11 +0000

    allow users to create new items and create item index page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,22 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create!(item_params)
+    redirect_to items_path, notice: "Item created!"
+  end
+
+  def edit
+
+  end
+
+  def item_params
+    params.require(:item).permit!
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 NkuProject::Application.routes.draw do
    resources :users
    resources :sessions
-
+   resources :items
    get '/signup' => 'users#new'
    get '/signin' => 'sessions#new'
    match '/signout', to: 'sessions#destroy', via: 'delete'
