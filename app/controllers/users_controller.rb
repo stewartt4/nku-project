52f69fe8ff23d5dd381aa51a7e13520945ee0674
Tyler Stewart <stewartt4@nku.edu>
2014-04-21 22:13:38 +0000

    fix form errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create!(user_params)
-    session[:user_id] = @user.id
-    redirect_to users_path, notice: "Welcome to Speedy Auction!"
+    @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to users_path, notice: "Welcome to Speedy Auction!"
+    else
+      render 'new'
+    end
   end
 
   def edit
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,8 @@
 class Item < ActiveRecord::Base
   belongs_to :user
   has_many :bids
+  validates :name, presence: true
+  validates :start_bid, presence: true
+  validates :picture, presence: true
   mount_uploader :picture
 end
